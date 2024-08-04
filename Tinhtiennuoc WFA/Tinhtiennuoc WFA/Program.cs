2e1925e5199f5a9140f090c84817a67c74f31445
dinh-quangha <dinhquangha08082005@gmail.com>
2024-08-04 19:42:47 +0700

    Update Program.cs

--- Tinhtiennuoc WFA/Tinhtiennuoc WFA/Program.cs
+++ Tinhtiennuoc WFA/Tinhtiennuoc WFA/Program.cs
@@ -11,7 +11,7 @@ namespace Tinhtiennuoc_WFA
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Information());
+            Application.Run(new Loggin());
         }
     }
 }
